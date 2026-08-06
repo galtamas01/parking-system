7a68dee232bd7aaf33b690a9e57f347df755a096
GT <galtamas777@gmail.com>
2026-08-06 17:27:58 +0300

    Database schema created with Prisma

---
+++ prisma/migrations/20260806135806_init/migration.sql
@@ -0,0 +1,45 @@
+-- CreateEnum
+CREATE TYPE "ReservationStatus" AS ENUM ('ACTIVE', 'CANCELLED');
+
+-- CreateEnum
+CREATE TYPE "SpotType" AS ENUM ('STANDARD', 'DISABLED', 'ELECTRIC');
+
+-- CreateTable
+CREATE TABLE "ParkingSpot" (
+    "id" SERIAL NOT NULL,
+    "code" TEXT NOT NULL,
+    "type" "SpotType" NOT NULL DEFAULT 'STANDARD',
+    "createdAt" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ParkingSpot_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Reservation" (
+    "id" SERIAL NOT NULL,
+    "parkingSpotId" INTEGER NOT NULL,
+    "startTime" TIMESTAMPTZ NOT NULL,
+    "endTime" TIMESTAMPTZ NOT NULL,
+    "status" "ReservationStatus" NOT NULL DEFAULT 'ACTIVE',
+    "createdAt" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Reservation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ParkingSpot_code_key" ON "ParkingSpot"("code");
+
+-- CreateIndex
+CREATE INDEX "Reservation_parkingSpotId_idx" ON "Reservation"("parkingSpotId");
+
+-- AddForeignKey
+ALTER TABLE "Reservation" ADD CONSTRAINT "Reservation_parkingSpotId_fkey" FOREIGN KEY ("parkingSpotId") REFERENCES "ParkingSpot"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+CREATE EXTENSION IF NOT EXISTS btree_gist;
+
+ALTER TABLE "Reservation"
+  ADD CONSTRAINT no_overlapping_active_reservations
+  EXCLUDE USING gist (
+    "parkingSpotId" WITH =,
+    tstzrange("startTime", "endTime", '[)') WITH &&
+  ) WHERE (status = 'ACTIVE');
